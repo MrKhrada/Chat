e79c451c97b7f51069b89eddd0be4428ed9bc447
Alan <alan@Contrasenas-MacBook-Pro.local>
2022-05-23 12:44:35 -0300

    added uploadImg save

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,31 +1,77 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Form, Button, Container, Row, Col} from 'react-bootstrap';
 import { Link } from "react-router-dom";
 import './Signup.css';
 import botImg from "../assets/bott.jpeg";
 
-
 function Signup() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [name, setName] = useState('');
+
+  //image upload states
+  const [image, setImage] = useState(null);
+  const [uploadingImg, setUploadingImg] = useState(false);
+  const [imagePreview, setImagePreview] = useState(null);
+
+  function validateImg(e) {
+    const file = e.target.files[0];
+    if(file.size >= 1048576) {
+      return alert("Max file size is 1mb");
+    } else {
+      setImage(file);
+      setImagePreview(URL.createObjectURL(file));
+    }
+  }
+
+  async function uploadImage(){
+    const data = new FormData();
+    data.append('file', image);
+    data.append('upload_preset', 'sg9ajkjs');
+    try {
+      setUploadingImg(true);
+      let res = await fetch('https://api.cloudinary.com/v1_1/alansilva/image/upload', {
+        method: 'post',
+        body: data,
+      })
+      const urlData = await res.json();
+      setUploadingImg(false); 
+      return urlData.url
+    } catch(error) {
+      setUploadingImg(false);
+      console.log(error)
+    }
+  }
+
+  async function handleSignup(e){
+    e.preventDefault();
+    if(!image) return alert('Please upload your profile picture');
+    const url = await uploadImage(image);
+    console.log(url);
+    // signup the user
+  }
+  
   return (
     <Container>
       <Row>
       <Col md={5} className="d-flex align-items-center justify-content-center flex-direction-column">
-          <Form style={{ width: "80%", maxWidth: 500 }}>
+          <Form style={{ width: "80%", maxWidth: 500 }} onSubmit={handleSignup}>
             <h1 className='text-center'>Create an account.</h1>
             <div className="signup-profile-pic__container">
-              <img src={botImg} alt="botImage" className='signup-profile-pic'/>
+              <img src={imagePreview || botImg} alt="botImage" className='signup-profile-pic'/>
               <label htmlFor="image-upload" className='image-upload-label'>
                 <i className="fas fa-plus-circle add-picture-icon"></i>
               </label>
+              <input type="file" id="image-upload" hidden accept="image/png, image/jpeg" onChange={validateImg} />
             </div>
             <Form.Group className="mb-3" controlId="formBasicName">
               <Form.Label>Name</Form.Label>
-              <Form.Control type="text" placeholder="Your name goes here." />
+              <Form.Control type="text" placeholder="Your name goes here." onChange={(e) => setName(e.target.value)} value={name} />
             </Form.Group>
             
             <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Control type="email" placeholder="Enter email" onChange={(e) => setEmail(e.target.value)} value={email}/>
               <Form.Text className="text-muted">
                 We'll never share your email with anyone else.
               </Form.Text>
@@ -33,13 +79,13 @@ function Signup() {
 
             <Form.Group className="mb-3" controlId="formBasicPassword">
               <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
+              <Form.Control type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} value={password}/>
             </Form.Group>
             <Form.Group className="mb-3" controlId="formBasicCheckbox">
               <Form.Check type="checkbox" label="Check me out" />
             </Form.Group>
             <Button variant="primary" type="submit">
-              Register
+              {uploadingImg ? 'Creating your account...': "Register"}
             </Button>
             <div className="py-4">
               <p className="text-center">
